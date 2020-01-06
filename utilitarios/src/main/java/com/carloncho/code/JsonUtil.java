ecb2abd4c72136610f23cf2e1454c771b1876b4d
carlos valenzuela <cvalenzuela@soaint.com>
2020-01-06 17:16:51 -0500

    Investigar lo de JSon

---
+++ utilitarios/src/main/java/com/carloncho/code/JsonUtil.java
@@ -0,0 +1,7 @@
+package com.carloncho.code;
+
+//https://blog.codota.com/how-to-convert-a-java-object-into-a-json-string
+//https://howtodoinjava.com/jackson/jackson-convert-object-to-from-json/
+public class JsonUtil {
+
+}
